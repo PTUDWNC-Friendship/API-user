4bdc210d8383534fd3360c393fae2781345cb271
Dinh Hai <dinhhhaii@gmail.com>
2019-12-09 15:41:52 +0700

    update user authentication

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 const passport = require('passport');
 var router = express.Router();
+const jwt = require('jsonwebtoken');
 const modelGenerator = require('../utils/model-generator');
+const constant = require('../utils/constant');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'UberForTutor' });
@@ -9,8 +11,15 @@ router.get('/', function(req, res, next) {
 
 /* GET user profile. */
 router.get('/me', passport.authenticate('jwt', {session: false}), function(req, res, next) {
-  const data = modelGenerator.toUserObject(req.authInfo);
-  res.json(data);
+  const authInfo = req.authInfo;
+  if(authInfo) {
+    var data = modelGenerator.toUserObject(req.authInfo);
+    data = { ...data, token: jwt.sign(JSON.stringify(data), constant.JWT_SECRET)}
+    res.json(data);
+  }
+  else {
+    res.json(null);
+  }
 });
 
 module.exports = router;
